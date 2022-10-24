aedf38364857d2916f73f5de09f86db6ef4fed49
lemonilemon <jaspertsai1019@gmail.com>
2022-10-24 19:20:21 +0800

    init

---
+++ prob/CSES/1160.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#ifdef local
+using std::cerr;
+#define debug(arg) deone(#arg, arg) 
+#define orange(l, r) derange(#l ", " #r, l, r)
+template <typename T> void deone(const char *s, T arg) {
+	cerr << "\033[1;32m" << s << " = " << arg << "\033[0m\n"; 
+}
+template <typename T> void derange(const char *s, T L, T R) {
+	cerr << "\033[1;32m[" << s << "] = [";
+	for(int i = 0; L != R; ++L) cerr << (i++? ", " : "") << *L;
+	cerr << "]\033[0m\n";
+}
+void safe() {
+	std::cerr<<"\033[1;32m"<<__PRETTY_FUNCTION__<<" is safe\033[0m\n";
+}
+#else
+#define safe() ((void)0)
+#define debug(...) ((void)0)
+#define orange(...) ((void)0)
+#endif // local
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T = 1;
+	//cin >> T;
+	for(int t = 1; t <= T; ++t) {
+		//cout << "Case " << t << ": ";
+		solve();
+	}
+}
+
