3608c6ab9513f760cba54931960368cd9ff0e409
lemonilemon <jaspertsai1019@gmail.com>
2023-03-16 00:17:27 +0800

    update

---
+++ prob/iscoj/4499.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m, q;
+	cin >> n >> m >> q;
+	for(int i = 0; i < q; ++i) {
+		cout << "YES\n";
+	}
+}
