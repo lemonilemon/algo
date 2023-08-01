e87c91d0ceb567dba99a77c8608415c6730d0f5a
lemonilemon <imlemonilemon@gmail.com>
2023-08-01 23:57:45 +0800

    update luogu

---
+++ prob/luogu/p1001.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    solve();
+}
