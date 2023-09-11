827c2803eea7d982b9fd0a4a9969b114f29be222
lemonilemon <imlemonilemon@gmail.com>
2023-09-11 14:45:06 +0800

    updatr judgegirl

--- prob/NTU/judgegirl/10367.c
+++ prob/NTU/judgegirl/10367.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-#include <stdbool.h>
-#define sqr(x) ((x) * (x))
+int sqr(int x) {
+    return x * x;
+}
 int main() {
     int xa, ya, ra, xb, yb, rb;
     scanf("%d%d%d%d%d%d", &xa, &ya, &ra, &xb, &yb, &rb);
-
-    bool ina = sqr(xa - , inb;
+    int x, y;
+    while(scanf("%d%d", &x, &y) != EOF) {
+        int adis2 = sqr(xa - x) + sqr(ya - y), bdis2 = sqr(xb - x) + sqr(yb - y);
+        if(adis2 > sqr(ra) && bdis2 > sqr(rb)) {
+            puts("Out");
+            continue;
+        }
+        if(adis2 < sqr(ra) || bdis2 < sqr(rb)) {
+            puts("In");
+            continue;
+        }
+        puts("On");
+    }
 }
