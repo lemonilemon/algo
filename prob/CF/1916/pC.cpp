3776ffaa2ae512b5ac964fa7ca3eb05d3263cced
lemonilemon <imlemonilemon@gmail.com>
2024-01-03 13:12:35 +0000

    update CF1916

---
+++ prob/CF/1916/pC.cpp
@@ -0,0 +1,74 @@
+/* 
+ * Created : 2023-12-30 15:15:11 lemonilemon
+ * Time complexity : O()
+ * Space complexity : O()
+ * Description : 
+ */
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+using ll = long long int;
+
+
+#define LOCAL
+
+// debug template
+#ifdef LOCAL
+#define debug(args...) _debug(#args, args)
+#define expand(arr, l, r) _expand(#arr, l, r, begin(arr) + l, begin(arr) + r)
+#define safe()  cerr << "\e[1;32m" << __PRETTY_FUNCTION__ << " at line " << __LINE__ << " is safe.\e[0m\n";
+template <typename ...T> void _debug(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    ((cerr << "\033[1;32m(" << s << ") = ("), ...,
+        (cerr << args << (--cnt ? ", " : ")\033[0m\n")));
+}
+template <typename T> void _expand(const char *s, int nl, int nr, T l, T r) {
+    cerr << "\033[1;32mexpand " << s << " from " << s << '[' << nl << "] to " << s << '[' << nr << "].\n---\n[";
+    for(T it = l; it != r + 1; ++it) {
+       cerr << *it << (it != r? ", " : "]\n---\033[0m\n");
+    }
+}
+#else
+#define debug(...) ((void)0) 
+#define safe() ((void)0) 
+#define expand(...) ((void)0)
+#endif
+
+// constants
+const int MAXN = 1e5 + 5;
+
+// solution
+void solve() {
+    int n;
+    cin >> n;
+    ll sum = 0;
+    int odd = 0, even = 0;
+    for(int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        if(a & 1) ++odd;
+        else ++even;
+        sum += a;
+        if(odd == 1 && even == 0) {
+            cout << sum << " \n"[i == n - 1];
+        } else {
+            int turn = odd / 3 + (odd % 3 == 1);
+            cout << sum - turn << " \n"[i == n - 1];
+        }
+    }
+}
+
+// main
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    for(int t = 1; t <= T; ++t) {
+        // cout << "Case #" << t << ":\n";
+        solve();
+    }
+    return 0;
+}
+
