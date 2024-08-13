429e980e8914fad1db51073f5dc55a871fff642e
lemonilemon <imlemonilemon@gmail.com>
2024-08-13 17:55:20 +0800

    Update: CSES/2181

--- prob/CSES/2181.cpp
+++ prob/CSES/2181.cpp
@@ -1,71 +1,91 @@
 #include <bits/stdc++.h>
+/* 
+ * Created : 2024-08-11 19:00:59 lemonilemon
+ * Time complexity : O()
+ * Space complexity : O()
+ * Description : 
+ */
+
+#include <bits/stdc++.h>
 #pragma GCC optimize("Ofast")
-#define local
-#ifdef local
-using std::cerr;
-#define debug(arg) deone(#arg, arg) 
-#define orange(l, r) derange(#l ", " #r, l, r)
-#define safe() std::cerr << "\033[1;32m" << __PRETTY_FUNCTION__ << " is safe\033[0m\n"
-template <typename T> void deone(const char *s, T arg) {
-	cerr << "\033[1;32m" << s << " = " << arg << "\033[0m\n"; 
-}
-template <typename T> void derange(const char *s, T L, T R) {
-	cerr << "\033[1;32m[" << s << "] = [";
-	for(int i = 0; L != R; ++L) cerr << (i++? ", " : "") << *L;
-	cerr << "]\033[0m\n";
-}
-#else
-#define safe() ((void)0)
-#define debug(...) ((void)0)
-#define orange(...) ((void)0)
-#endif // local
 using namespace std;
 using ll = long long int;
-FILE* setIO(string file = "") {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	if(file.empty()) return NULL;
-	if(freopen((file + ".in").c_str(), "r", stdin) == NULL) return NULL; 
-	return freopen((file + ".out").c_str(), "w", stdout);
+
+
+#define LOCAL
+
+// debug template
+#ifdef LOCAL
+#define debug(args...) _debug(#args, args)
+#define expand(arr, l, r) _expand(#arr, l, r, begin(arr) + l, begin(arr) + r)
+#define safe()  cerr << "\e[1;32m" << __PRETTY_FUNCTION__ << " at line " << __LINE__ << " is safe.\e[0m\n";
+template <typename ...T> void _debug(const char *s, T ...args) {
+    int cnt = sizeof...(T);
+    ((cerr << "\033[1;32m(" << s << ") = ("), ...,
+        (cerr << args << (--cnt ? ", " : ")\033[0m\n")));
 }
-const int MAXN = 10, MAXM = 1005, MOD = 1e9 + 7;
+template <typename T> void _expand(const char *s, int nl, int nr, T l, T r) {
+    cerr << "\033[1;32mexpand " << s << " from " << s << '[' << nl << "] to " << s << '[' << nr << "].\n---\n[";
+    for(T it = l; it != r + 1; ++it) {
+       cerr << *it << (it != r? ", " : "]\n---\033[0m\n");
+    }
+}
+#else
+#define debug(...) ((void)0) 
+#define safe() ((void)0) 
+#define expand(...) ((void)0)
+#endif
+
+// constants
+const int MOD = 1e9 + 7;
+const int MAXN = 10;
+const int MAXM = 1005;
 int mabs(ll a) {
-	return (a % MOD + MOD) % MOD;
+    return (a % MOD + MOD) % MOD;
 }
 int madd(int a, int b) {
-	return mabs(a + b);
+    return mabs(a + b);
 }
-int mull(int a, int b) {
-	return mabs(1ll * a * b);
+int mmul(int a, int b) {
+    return mabs(1ll * a * b);
 }
-int dp[2][1 << MAXN];
+
+int dp[2][1 << MAXN]; // dp[0]: current, dp[1]: previous
+
+
+
+// solution
 void solve() {
-	int n, m;
-	cin >> n >> m;
-	dp[0][0] = 1;
-	for(int i = 1; i <= m; ++i) {
-		for(int j = 0; j < (1 << n); ++j) {
-			for(int k = 0; k < n - 1; ++k) {
-				if(((1 << k) & j) && ((1 << (k + 1)) & j)) {
-					dp[i & 1][j] = madd(dp[i & 1][j], dp[!(i & 1)][j ^ (1 << k) ^ (1 << (k + 1))]);
-				}
-			}
-			for(int k = 0; k < n; ++k) {
-				if((1 << k) & j) {
-					dp[i & 1][j] = madd(dp[i & 1][j], dp[!(i & 1)][j ^ (1 << k)]);
-				}
-			}
-		}
-	}
+    int n, m;
+    cin >> n >> m;
+    dp[1][(1 << n) - 1] = 1;
+    for(int j = 0; j < m; ++j) {
+        for(int i = 0; i < n; ++i) {
+            for(int s = 0; s < 1 << n; ++s) {
+                dp[0][s] = dp[1][s ^ 1 << i];
+                // vertical
+                if(i && (s >> (i - 1) & 3) == 3) {
+                    dp[0][s] = madd(dp[0][s], dp[1][s ^ 1 << (i - 1)]);
+                }
+                // horizontal
+            }
+            for(int s = 0; s < 1 << n; ++s) {
+                dp[1][s] = dp[0][s];
+            }
+        }
+    }
+    cout << dp[1][(1 << n) - 1] << '\n';
 }
 
+// main
 int main() {
-	setIO();
-	int T = 1;
-	//cin >> T;
-	for(int t = 1; t <= T; ++t) {
-		//cout << "Case " << t << ": ";
-		solve();
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    // cin >> T;
+    for(int t = 1; t <= T; ++t) {
+        // cout << "Case #" << t << ":\n";
+        solve();
+    }
+    return 0;
 }
-
