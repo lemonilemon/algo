035e690b1fb2729a7ee33070bc7176f0e557c8a1
lemonilemon <imlemonilemon@gmail.com>
2026-02-13 00:05:23 +0800

    contest: CF 2173

---
+++ prob/CF/2173/pB.cpp
@@ -0,0 +1,74 @@
+/*
+ * Created : 2026-02-06 20:23:43 lemonilemon
+ * Time complexity : O()
+ * Space complexity : O()
+ * Description :
+ */
+
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+using namespace std;
+using ll = long long int;
+
+#define LOCAL
+
+// debug template
+#ifdef LOCAL
+#define debug(args...) _debug(#args, args)
+#define expand(arr, l, r) _expand(#arr, l, r, begin(arr) + l, begin(arr) + r)
+#define safe()                                                                 \
+    cerr << "\e[1;32m" << __PRETTY_FUNCTION__ << " at line " << __LINE__       \
+         << " is safe.\e[0m\n";
+template <typename... T> void _debug(const char *s, T... args) {
+    int cnt = sizeof...(T);
+    ((cerr << "\033[1;32m(" << s << ") = ("), ...,
+     (cerr << args << (--cnt ? ", " : ")\033[0m\n")));
+}
+template <typename T> void _expand(const char *s, int nl, int nr, T l, T r) {
+    cerr << "\033[1;32mexpand " << s << " from " << s << '[' << nl << "] to "
+         << s << '[' << nr << "].\n---\n[";
+    for (T it = l; it != r + 1; ++it) {
+        cerr << *it << (it != r ? ", " : "]\n---\033[0m\n");
+    }
+}
+#else
+#define debug(...) ((void)0)
+#define safe() ((void)0)
+#define expand(...) ((void)0)
+#endif
+
+// constants
+const int MAXN = 1e5 + 5;
+ll mndp, mxdp;
+int a[MAXN], b[MAXN];
+// solution
+void solve() {
+    int n;
+    cin >> n;
+    mndp = mxdp = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> b[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        ll last_mxdp = mxdp, last_mndp = mndp;
+        mxdp = max(last_mxdp - a[i], b[i] - last_mndp);
+        mndp = min(last_mndp - a[i], b[i] - last_mxdp);
+    }
+    cout << mxdp << '\n';
+}
+
+// main
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; ++t) {
+        // cout << "Case #" << t << ":\n";
+        solve();
+    }
+    return 0;
+}
